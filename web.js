ee16cebc93a8f1248ca831f01f6a2f46c9d9e3e6
Alexandre Fonseca <alexandrejorgefonseca@gmail.com>
2013-07-09 11:47:41 +0100

    Changed encoding parametrization to work on Heroku

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var index = fs.readFileSync('index.html', {'encoding': 'utf8'})
+  var index = fs.readFileSync('index.html', 'utf8')
   response.send(index);
 });
 
